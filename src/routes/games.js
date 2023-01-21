add66b7f571408c364c54ecdb70a591424058e22
Cinthia Gisela Camacho <gis25cam@gmail.com>
2023-01-20 21:54:47 -0600

    add crud methods

---
+++ src/routes/games.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const gamesController = require('../controllers/games');
+
+router.get('/', gamesController.getAll);
+
+router.get('/:id', gamesController.getSingle);
+
+router.post('/', gamesController.createGames);
+
+router.put('/:id', gamesController.updateGames);
+
+router.delete('/:id', gamesController.deleteGames);
+
+module.exports = router;
---
+++ src/routes/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+router.use('/games', require('./games'))
+
+module.exports = router;
